1ba55632033418a49ab9d64001bc7c27b5f03bae
raghuram balaji <balajiraghuram92@gmail.com>
2025-03-03 11:44:28 +0530

    MVC-.net9-working prototype

--- WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/Controllers/CategoryController.cs
@@ -31,11 +31,63 @@ namespace WebApplication1.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created succussfully";
                 return RedirectToAction("Index");
             }
 
             return View();
            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var categ = _db.Categories.Find(id);
+            if (categ == null)
+                return NotFound();
+
+            return View(categ);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated succussfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var categ = _db.Categories.FirstOrDefault(recd => recd.Id == id);
+            if (categ == null)
+                return NotFound();
+
+            return View(categ);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            var categ = _db.Categories.FirstOrDefault(recd => recd.Id == id);
+            if (categ == null)
+                return NotFound();
+
+            _db.Categories.Remove(categ);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted succussfully";
+            return RedirectToAction("Index");
         }
     }
 }
